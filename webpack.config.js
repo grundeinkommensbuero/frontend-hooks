8a77370a06311f6339cbf6233936e0d196ede1d0
Valioesi <Vali_Schagerl@web.de>
2020-09-28 17:51:49 +0200

    Setup: webpack, babel, eslint

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    target: 'web',
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
